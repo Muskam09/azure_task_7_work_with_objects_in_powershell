7a174bba53f7dff8de1e793246b9f9213d0fee8e
Maksimens/Oriflame <Muskam.dan@gmail.com>
2025-07-14 20:36:13 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,26 @@
-# Write your code here
+$targetsize = "Standard_B2pts_v2"
+$dataFolder = "data"
+
+
+$jsonFiles = Get-ChildItem -Path $dataFolder -Filter *.json
+
+$MatchesRegions = @()
+
+foreach($file in $jsonFiles){
+  $filePath = $file.FullName
+
+  $vmParameters = Get-content $filePath | ConvertFrom-Json
+
+  $found = $vmParameters | Where-Object {$_.name -eq $targetsize}
+
+  if ($found) {
+    $region = $file.BaseName
+    $MatchesRegions += $region
+  }
+}
+
+
+$MatchesRegions | ConvertTo-Json | Set-Content -Path "result.json"
+
+
+Write-Host "Результат експортовано до result.json"
